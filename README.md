26fc7c30c04c0a08515d6a1c149f23fed9e2ff70
makicamel <unright@gmail.com>
2020-10-11 18:17:07 +0900

    Add deploy script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
 My dotfiles
+
+## Install
+
+```zsh
+git clone https://github.com/makicamel/dotfiles
+cd dotfiles
+bin/deploy.sh
+```
---
+++ bin/deploy.sh
@@ -0,0 +1,11 @@
+DOT_DIR=$(pwd)
+BIN_DIR=$(cd $(dirname $0); pwd)
+
+ln -s $DOT_DIR/.gemrc      ~/.gemrc
+ln -s $DOT_DIR/.vimrc      ~/.vimrc
+ln -s $DOT_DIR/.gitconfig  ~/.gitconfig
+
+ln -s $BIN_DIR/.alias.sh   ~/dev/bin/.alias.sh
+ln -s $BIN_DIR/git-open-pr.sh   ~/dev/bin/git-open-pr.sh
+
+echo 'Symbolic link created.'
