83a33b35d1538230773a693ff713b14d8529f8d1
makicamel <unright@gmail.com>
2026-02-07 17:27:30 +0900

    openocd

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,6 @@
 DOT_DIR=$(pwd)
 BIN_DIR=$(cd $(dirname $0); pwd)
+OPENOCD_DIR=$(cd openocd && pwd)
 
 ln -s $DOT_DIR/.gemrc      ~/.gemrc
 ln -s $DOT_DIR/.vimrc      ~/.vimrc
@@ -10,5 +11,9 @@ ln -s $DOT_DIR/karabiner.json   ~/.config/karabiner/karabiner.json
 
 ln -s $BIN_DIR/.alias.sh   ~/dev/bin/.alias.sh
 ln -s $BIN_DIR/git-open-pr.sh   ~/dev/bin/git-open-pr.sh
+
+mkdir ~/openocd
+ln -s $OPENOCD_DIR/openocd-rp2040.cfg ~/openocd/rp2040.cfg
+ln -s $OPENOCD_DIR/openocd-rp2350.cfg ~/openocd/rp2350.cfg
 
 echo 'Symbolic link created.'
